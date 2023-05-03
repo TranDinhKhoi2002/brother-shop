d18a93b8852ef655c648cb2bad21d693be37f196
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-05-03 17:22:40 +0700

    add facebook chat plugin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from '@/common/utility/createEmotionCache';
+import FacebookPluginChat from '@/common/components/Plugin/FacebookPluginChat';
 
 export default class MyDocument extends Document {
   render() {
@@ -11,6 +12,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <FacebookPluginChat />
         </body>
       </Html>
     );
