5cb98c2a0d4568fd7019edc3a716db6a3afab426
Tran Dinh Khoi <trandinhkhoi102@gmail.com>
2024-03-15 16:36:27 +0700

    #1: add route change event listeners

--- src/common/components/Header/Search/index.tsx
+++ src/common/components/Header/Search/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { isEmpty } from 'lodash';
@@ -6,7 +6,6 @@ import { Hits, SearchBox, useHits, useSearchBox } from 'react-instantsearch';
 import { Box, Card, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-import BackdropLoading from '@/common/components/Loading/BackdropLoading';
 import NoSearchResult from '@/modules/search/NoSearchResult';
 import config from '@/config';
 import CustomHit from './Hit';
@@ -17,67 +16,56 @@ type SearchInputProps = {
 };
 
 function SearchInput({ className, closeSearch }: SearchInputProps): ReactElement {
-  const [loading, setLoading] = useState(false);
   const { query } = useSearchBox();
   const { hits } = useHits();
   const router = useRouter();
 
-  useEffect(() => {
-    if (router.pathname === config.routes.search) {
-      setLoading(false);
-      closeSearch();
-    }
-  }, [router.pathname, closeSearch]);
-
   const handleSearchSubmit = () => {
     if (isEmpty(query)) return;
     router.push({ pathname: config.routes.search, query: { keyword: query } });
-    setLoading(true);
+    closeSearch();
   };
 
   return (
-    <>
-      <Box className={`${className} !bg-white`}>
-        <SearchBox
-          placeholder="Tìm kiếm sản phẩm"
-          autoFocus={true}
-          spellCheck={false}
-          classNames={{
-            root: 'w-full',
-            form: 'w-full',
-            input: 'w-[90%] border-none outline-none text-xl lg:text-[40px]',
-            submitIcon: 'ml-3',
-            resetIcon: 'hidden',
-          }}
-          onSubmit={handleSearchSubmit}
-          submitIconComponent={() => <SearchIcon sx={{ fontSize: '30px', mb: '12px' }} />}
-        />
-        <Card elevation={5} className="absolute top-20 left-4 right-4 z-[1000] bg-white">
-          <Typography sx={{ textAlign: 'end', mt: 3, mr: 3, cursor: 'pointer' }} onClick={closeSearch}>
-            Đóng
-          </Typography>
-          {hits.length > 0 ? (
-            <Box className="max-h-[65vh] overflow-y-scroll">
-              <Hits hitComponent={({ hit }) => <CustomHit hit={hit} onSelect={closeSearch} />} className="px-8" />
+    <Box className={`${className} !bg-white`}>
+      <SearchBox
+        placeholder="Tìm kiếm sản phẩm"
+        autoFocus={true}
+        spellCheck={false}
+        classNames={{
+          root: 'w-full',
+          form: 'w-full',
+          input: 'w-[90%] border-none outline-none text-xl lg:text-[40px]',
+          submitIcon: 'ml-3',
+          resetIcon: 'hidden',
+        }}
+        onSubmit={handleSearchSubmit}
+        submitIconComponent={() => <SearchIcon sx={{ fontSize: '30px', mb: '12px' }} />}
+      />
+      <Card elevation={5} className="absolute top-20 left-4 right-4 z-[1000] bg-white">
+        <Typography sx={{ textAlign: 'end', mt: 3, mr: 3, cursor: 'pointer' }} onClick={closeSearch}>
+          Đóng
+        </Typography>
+        {hits.length > 0 ? (
+          <Box className="max-h-[65vh] overflow-y-scroll">
+            <Hits hitComponent={({ hit }) => <CustomHit hit={hit} onSelect={closeSearch} />} className="px-8" />
 
-              {query && (
-                <Link
-                  href={`${config.routes.search}?keyword=${query}`}
-                  className="text-center block font-medium mb-5"
-                  onClick={closeSearch}
-                >
-                  Xem tất cả
-                  <KeyboardDoubleArrowRightIcon />
-                </Link>
-              )}
-            </Box>
-          ) : (
-            <NoSearchResult keyword={query} />
-          )}
-        </Card>
-      </Box>
-      <BackdropLoading isVisible={loading} />
-    </>
+            {query && (
+              <Link
+                href={`${config.routes.search}?keyword=${query}`}
+                className="text-center block font-medium mb-5"
+                onClick={closeSearch}
+              >
+                Xem tất cả
+                <KeyboardDoubleArrowRightIcon />
+              </Link>
+            )}
+          </Box>
+        ) : (
+          <NoSearchResult keyword={query} />
+        )}
+      </Card>
+    </Box>
   );
 }
 
--- src/common/components/Layout/MainLayout.tsx
+++ src/common/components/Layout/MainLayout.tsx
@@ -11,6 +11,7 @@ import BackdropLoading from '../Loading/BackdropLoading';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
 import useDrawer from '@/hooks/useDrawer';
 import CartDrawer from '@/modules/cart/components/CartDrawer';
+import { useRouter } from 'next/router';
 
 type LayoutProps = {
   children: ReactNode;
@@ -21,39 +22,55 @@ function Layout(props: LayoutProps): ReactElement {
   const { render: renderSidebar, onOpen: onOpenSidebar } = useDrawer(Sidebar);
   const { render: renderWishlistDrawer, onOpen: onOpenWishlistDrawer } = useDrawer(WishlistDrawer);
 
-  const [loaded, setLoaded] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
+  const router = useRouter();
 
   useEffect(() => {
-    setLoaded(true);
-  }, []);
+    const handleRouteChangeStart = (url: string) => url !== router.asPath && setLoading(true);
+    const handleRouteChangeComplete = (url: string) => url === router.asPath && setLoading(false);
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeComplete);
+    };
+  }, [router.asPath, router.events]);
 
   useEffect(() => {
     dispatch(fetchCommonData());
   }, [dispatch]);
 
-  if (!loaded) {
-    return <BackdropLoading isVisible={!loaded} />;
-  }
-
   return (
-    <Box sx={{ position: 'relative' }}>
-      <Header showSideBar={onOpenSidebar} showCartPreview={onOpenCartDrawer} showWishlist={onOpenWishlistDrawer} />
-      <Box>{props.children}</Box>
-      <Fab color="secondary" aria-label="add" href="tel:0349175927" sx={{ position: 'fixed', bottom: 100, right: 24 }}>
-        <AddIcon />
-      </Fab>
-      <MessengerCustomerChat
-        pageId={process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID}
-        appId={process.env.NEXT_PUBLIC_FACEBOOK_ID}
-        htmlRef={process.env.NEXT_PUBLIC_BASE_URL}
-        language="vi_VN"
-      />
-      <Footer />
-      {renderSidebar()}
-      {renderCartDrawer()}
-      {renderWishlistDrawer()}
-    </Box>
+    <>
+      <Box sx={{ position: 'relative' }}>
+        <Header showSideBar={onOpenSidebar} showCartPreview={onOpenCartDrawer} showWishlist={onOpenWishlistDrawer} />
+        <Box>{props.children}</Box>
+        <Fab
+          color="secondary"
+          aria-label="add"
+          href="tel:0349175927"
+          sx={{ position: 'fixed', bottom: 100, right: 24 }}
+        >
+          <AddIcon />
+        </Fab>
+        <MessengerCustomerChat
+          pageId={process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID}
+          appId={process.env.NEXT_PUBLIC_FACEBOOK_ID}
+          htmlRef={process.env.NEXT_PUBLIC_BASE_URL}
+          language="vi_VN"
+        />
+        <Footer />
+        {renderSidebar()}
+        {renderCartDrawer()}
+        {renderWishlistDrawer()}
+      </Box>
+      <BackdropLoading isVisible={loading} />
+    </>
   );
 }
 
